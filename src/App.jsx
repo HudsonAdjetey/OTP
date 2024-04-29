51c18c24a912c153eeb03e2f9b8ab2bd3e587c24
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-29 11:34:44 -0700

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import PhoneOtpLogin from "./components/PhoneOtpLogin";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <h1>Login With Phone</h1>
+      <PhoneOtpLogin />
+    </div>
+  );
 };
 
 export default App;
